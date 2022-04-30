0abe45d4678fff9766c860054ad1208b73adc2fa
Alejao03 <alejao03@ucm.es>
2022-04-30 11:10:04 +0200

    Probado ejercicio 4 y ejercicio 5
    Co-Authored-By: davidrodriguezz <62748674+davidrodriguezz@users.noreply.github.com>

--- Practica 2-2/practica2.2/replicacion-chat/Chat.cc
+++ Practica 2-2/practica2.2/replicacion-chat/Chat.cc
@@ -1,3 +1,4 @@
+//David Rodríguez y Alejandro Ortega
 #include "Chat.h"
 
 // -----------------------------------------------------------------------------
@@ -10,15 +11,37 @@ void ChatMessage::to_bin()
     memset(_data, 0, MESSAGE_SIZE);
 
     //Serializar los campos type, nick y message en el buffer _data
+    char* punteroBuffer = _data;
+
+    memcpy(punteroBuffer, &type, sizeof(uint8_t));
+    punteroBuffer += sizeof(uint8_t);
+
+    memcpy(punteroBuffer, nick.c_str(), 8 * sizeof(char));
+    punteroBuffer += 8 * sizeof(char);
+
+    memcpy(punteroBuffer, message.c_str(), 80 * sizeof(char));
 }
 
-int ChatMessage::from_bin(char * bobj)
+int ChatMessage::from_bin(char* bobj)
 {
     alloc_data(MESSAGE_SIZE);
 
-    memcpy(static_cast<void *>(_data), bobj, MESSAGE_SIZE);
+    memcpy(static_cast<void>(_data), _obj, MESSAGE_SIZE);
 
     //Reconstruir la clase usando el buffer _data
+    char punteroBuffer = _data;
+
+    memcpy(&type, punteroBuffer, sizeof(uint8_t));
+    punteroBuffer += sizeof(uint8_t);
+
+    char nombre[8];
+    mempcpy(&nombre, punteroBuffer, 8 * sizeof(char));
+    punteroBuffer += 8 * sizeof(char);
+    nick = nombre;
+
+    char mensaje[80];
+    memcpy(&mensaje, punteroBuffer, 80 * sizeof(char));
+    message = mensaje;
 
     return 0;
 }
@@ -31,15 +54,15 @@ void ChatServer::do_messages()
     while (true)
     {
         /*
-         * NOTA: los clientes están definidos con "smart pointers", es necesario
-         * crear un unique_ptr con el objeto socket recibido y usar std::move
-         * para añadirlo al vector
-         */
+            *NOTA: los clientes están definidos con "smart pointers", es necesario
+            * crear un unique_ptr con el objeto socket recibido y usar std::move
+            * para añadirlo al vector
+            * */
 
-        //Recibir Mensajes en y en función del tipo de mensaje
-        // - LOGIN: Añadir al vector clients
-        // - LOGOUT: Eliminar del vector clients
-        // - MESSAGE: Reenviar el mensaje a todos los clientes (menos el emisor)
+            //Recibir Mensajes en y en función del tipo de mensaje
+            // - LOGIN: Añadir al vector clients
+            // - LOGOUT: Eliminar del vector clients
+            // - MESSAGE: Reenviar el mensaje a todos los clientes (menos el emisor)
     }
 }
 
@@ -72,10 +95,9 @@ void ChatClient::input_thread()
 
 void ChatClient::net_thread()
 {
-    while(true)
+    while (true)
     {
         //Recibir Mensajes de red
         //Mostrar en pantalla el mensaje de la forma "nick: mensaje"
     }
 }
-
